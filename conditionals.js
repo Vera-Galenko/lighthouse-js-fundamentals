f66392ee9a5847f24ab219355b8002552948b8cd
Vera-Galenko <vera.galenko@icloud.com>
2020-08-11 20:21:09 +0000

    changed the code

--- conditionals.js
+++ conditionals.js
@@ -1,18 +1,28 @@
+// var x = 1;
 
-var x = 1;
-
-// while loop with a stop condition
-while (x <= 20) {
-    if (x % 3 === 0 && x % 5 === 0) {
-        console.log("JuliaJames");
-    }else if (x % 5 === 0) {
-        console.log("James");
-    }else if (x % 3 === 0) {
-        console.log("Julia");
+// // while loop with a stop condition
+// while (x < 21) {
+//     if (x % 3 === 0 && x % 5 === 0) {
+//         console.log("JuliaJames");
+//     }else if (x % 5 === 0) {
+//         console.log("James");
+//     }else if (x % 3 === 0) {
+//         console.log("Julia");
+//     }
+//     else {
+//         console.log(x);
+//     }
+//     // increment x at the end of each loop
+//     x = x + 1 ; 
+// }
+let age = 13;
+const whichSchool = function (age) {
+    if (age < 13) {
+        console.log("Elementary School");
+    } else if (age >= 13 && age <= 18) {
+        console.log("Secondary School");
+    } else {
+        console.log("Lighthouse Labs");
     }
-    else {
-        console.log(x);
-    }
-    // increment x at the end of each loop
-    x = x + 1 ; 
 }
+whichSchool(age);
