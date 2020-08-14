700dc3011d8d0765078c53a0edab2b0e01f2acf6
Vera-Galenko <vera.galenko@icloud.com>
2020-08-14 20:27:46 +0000

    message

---
+++ hello.js
@@ -0,0 +1,17 @@
+const sayHello  = function (name) {
+    console.log("Hello," + name);
+  }
+  
+  sayHello("Vera!");
+
+  const returnSayHello  = function (name) {
+    return "Hello, " + name;
+  }
+  const greeting = returnSayHello("John");
+
+
+  const returnSayHello  = function (name) {
+    return "Hello, " + name;
+  }
+  const greeting = returnSayHello('John');
+  console.log(greeting);
